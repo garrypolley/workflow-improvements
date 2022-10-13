84fd7cce5e55bcc11def633df5574e4e8b75c8c1
Nate <nathan.ferreira@unit21.ai>
2022-10-13 10:56:22 -0400

    chore: add comment for format specifier

--- git-commands/git-cart.sh
+++ git-commands/git-cart.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
 cd $ORG_GIT_ROOT
+
+# This prints the header for the table. The weird format specifiers
+# are to make the spacing look nice and trim if `Repo` or
+# `Current Branch` are too long
 printf "| %-20.20s | %-30.30s |\n" "Repo" "Current Branch"
 printf "\n"
 for dir in $(ls -d */) 
